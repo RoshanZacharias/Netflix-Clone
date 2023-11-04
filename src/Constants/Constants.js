5b809acc4df71362d655c143aecb7a3c77e2d6ea
Roshan Zacharias <roshanzacharias20@gmail.com>
2023-11-04 19:34:27 +0530

    first commit

---
+++ src/Constants/Constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = '08cd4e89558871e4e818b0e9d70c64d2';
+export const imageUrl = 'https://image.tmdb.org/t/p/original';
---
+++ src/urls.js
@@ -0,0 +1,6 @@
+import {API_KEY} from './Constants/Constants'
+
+export const originals = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213`
+export const action = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=28`
+export const romance = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=10749`
+export const comedy = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=35`
